86e5082bb7d0990f9807931b25b946df027e5808
Mijung Lee <mijung.m@gmail.com>
2018-03-23 12:55:04 -0700

    new header and removed dead code in js

--- js/header.js
+++ js/header.js
@@ -1,65 +1,18 @@
 let windowSize = window.innerWidth;
 
-// Open Nav Menu
+// Open Nav Menu 
 function openMenu() {
-  if (windowSize > 1025) {
-    document.getElementById('menu-overlay').style.width = '40%';    
-  } else if (windowSize < 1025){
-    document.getElementById('menu-overlay').style.width = '100%';
-    document.documentElement.style.overflow = 'hidden';    
-  }
+  document.getElementById('menu-overlay').style.width = '100%';
+  document.documentElement.style.overflow = 'hidden';    
   document.getElementById('menu-close-wrapper').style.opacity = '1';
   document.getElementById('menu-content').style.opacity = '1';  
 }
-
 // Close Nav Menu
 function closeMenu() {
-  if (windowSize > 1025) {
-    closeForm();
-    setTimeout(function() {
-      document.getElementById('menu-overlay').style.width = '0%';
-      document.getElementById('menu-close-wrapper').style.opacity = '0';  
-      document.getElementById('menu-content').style.opacity = '0';
-    }, 300);
-  } else if (windowSize < 1025){
-    document.getElementById('menu-overlay').style.width = '0%';
-    document.getElementById('menu-close-wrapper').style.opacity = '0';  
-    document.getElementById('menu-content').style.opacity = '0';
-    document.documentElement.style.overflow = '';  
-  }
-}
-
-// Open Contact Form
-function openForm() {
-  if (windowSize > 1025) {
-    openMenu();
-    setTimeout(function() {
-      document.getElementById('contact-form').style.width = '100%';
-      document.documentElement.style.overflow = 'hidden';                 
-      // document.getElementById('contact-form-content').style.right = '0';  
-      setTimeout(function() {
-        document.getElementById('contact-form-content').style.opacity = '1';        
-      }, 300);
-    }, 300);
-  } else if (windowSize < 1025) {
-  document.getElementById('contact-form').style.width = '100%';
-  document.getElementById('contact-close-wrapper').style.opacity = '1';
-  // document.getElementById('contact-form-content').style.right = '0'; 
-  document.getElementById('contact-form-content').style.opacity = '1';  
-  document.documentElement.style.overflow = 'hidden';   
-    if (document.getElementById('menu-overlay').style.width === '100%') {
-      document.getElementById('menu-overlay').style.width = '0%'  
-    }        
-  }
-}
-
-// Close Contact Form
-function closeForm() {
-  document.getElementById('contact-form').style.width = '0%';
-  document.getElementById('contact-close-wrapper').style.opacity = '0';  
-  // document.getElementById('contact-form-content').style.right = '-800px';
-  document.getElementById('contact-form-content').style.opacity = '0';
-  document.documentElement.style.overflow = '';            
+  document.getElementById('menu-overlay').style.width = '0%';
+  document.getElementById('menu-close-wrapper').style.opacity = '0';  
+  document.getElementById('menu-content').style.opacity = '0';
+  document.documentElement.style.overflow = '';  
 }
 
 jQuery(document).ready(function($) {
@@ -98,49 +51,16 @@ jQuery(document).ready(function($) {
 
   // Sticky Header
   // Keep track of scroll up or down
-  // (function stickyHeader () {
-    let lastScroll = 0;
-    $(window).scroll(function(e) {
-      let st = $(this).scrollTop();
-      if (window.scrollY > 1) {
-        if (st > lastScroll) {
-          $('header').css('top', '-110px');   
-        } else {
-          $('header').css('top', 0);
-        }
-        lastScroll = st;
+  let lastScroll = 0;
+  $(window).scroll(function(e) {
+    let st = $(this).scrollTop();
+    if (window.scrollY > 1) {
+      if (st > lastScroll) {
+        $('header').css('top', '-110px');   
+      } else {
+        $('header').css('top', 0);
       }
-    });
-  // })();
-
-  // Add WP Forms classes (built in CSS)for desktop layout 
-  function contactFormMediaQuery() {
-    if (windowSize > 1025) {
-      $('.contact__first-name').addClass('wpforms-one-half wpforms-first');
-      $('.contact__last-name').addClass('wpforms-one-half');
-      $('.contact__email').addClass('wpforms-one-half wpforms-first');
-      $('.contact__phone').addClass('wpforms-one-half');
-    } else {
-      $('.contact__first-name').removeClass('wpforms-one-half wpforms-first');
-      $('.contact__last-name').removeClass('wpforms-one-half');
-      $('.contact__email').removeClass('wpforms-one-half wpforms-first');
-      $('.contact__phone').removeClass('wpforms-one-half');
+      lastScroll = st;
     }
-  }
-
-  contactFormMediaQuery();
-
-  $(window).resize(function() {
-    contactFormMediaQuery();
   });
-
-  // Confirmation message from WPFORMS
-  if ($('.wpforms-confirmation-container-full').length) {
-    document.getElementById('contact-form').style.transition = 'none';
-    openForm();
-    setTimeout(function() {
-      document.getElementById('contact-form').style.transition = 'width 0.7s ease-in-out';      
-      closeMenu();
-    }, 3000);
-  }
 });
