49ff7d49dc7339aa4597abc7f1b0b7e2a288807d
TuanPham303 <phamcongtuan303@gmail.com>
2018-03-26 16:02:02 -0700

    remove alert and fix single project style

--- js/header.js
+++ js/header.js
@@ -41,14 +41,12 @@ jQuery(document).ready(function($) {
     let interval = setInterval(loaderAnimation, 2000);
     // On window load, play animation once and fade out
     $(window).on('load', function() {
-      alert('loaded');
       clearInterval(interval);
       loaderAnimation();
       setTimeout(function() {
         $('#loader').fadeOut();      
       }, 1500);
     });
-    alert('after loaded');
   })();
 
   // Sticky Header
